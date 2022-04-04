74992232c03f958c770244ae48af56fea5f123a5
Patryk Janicki <patrykjanicki@icloud.com>
2022-04-04 19:30:32 +0200

    basic project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css"
+import Header from "./Components/Header/Header"
+import Menu from "./Components/Menu/Menu"
+import RockFinder from "./Components/RockFinder/RockFinder"
+import RoutesList from "./Components/RoutesList/RoutesList"
+import Map from "./Components/Map/Map"
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>hello</h1>
+			<Menu />
+			<Header />
+			<RockFinder />
+			<RoutesList />
+			<Map/>
 		</div>
 	)
 }
---
+++ src/Components/RockFinder/RockFinder.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function RockFinder() {
+	return <div>To jest wyszukiwarka skał</div>
+}
+
+export default RockFinder
